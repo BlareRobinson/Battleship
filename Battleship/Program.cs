3b972537918bec53a569ed29540adbd4f57e1487
Blare Robinson <berobi01@louisville.edu>
2021-04-08 19:45:11 -0400

    Add Battleship Repo.

---
+++ Battleship/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+namespace Battleship
+{
+    class Program
+    {
+
+        static void Calculate()
+        {
+            var number = 8;
+            Console.WriteLine(number + 3);
+
+            number = 20;
+            Console.WriteLine(number - 8);
+        }
+
+        static void Sleep()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Done sleeping!");
+
+            Calculate();
+        }
+
+        static void Main(string[] args)
+        {
+            
+            var greeting = "Welcome to Battleship";
+            Console.WriteLine(greeting);
+
+            Calculate();
+            Sleep();
+
+        }
+    }
+}
